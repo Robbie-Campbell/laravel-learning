90078a995652744a2dc9bdc7b85333a4bdb55870
robbie <robbielcampbell@hotmail.com>
2020-10-05 18:08:20 +0100

    neater postcontroller

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table("posts")->where("slug", $slug)->first();
-
 
         return view("post", [
-            "post" => $post
+            "post" => Post::where("slug", $slug)->firstOrFail()
         ]);
     }
 }
